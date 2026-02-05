b99703015c2dbb82f2807a2b13d6beb2ba8a0825
Mr. Scripter <hitlerbeam@gmail.com>
2026-02-05 20:46:55 +0800

    Create generate.js

---
+++ api/api/generate.js
@@ -0,0 +1,12 @@
+import { keys } from "./_store.js";
+
+export default function handler(req, res) {
+  const key =
+    "NIK-" + Math.random().toString(36).slice(2, 10).toUpperCase();
+
+  keys[key] = {
+    createdAt: Date.now()
+  };
+
+  res.status(200).send(key);
+}
